150ab4a4f788c1aeb91d4ab07ebe05e3db18cf70
mulangren <wangyutao52135@163.com>
2016-11-27 14:04:42 +0800

    增加本地测试

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -21,6 +21,7 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
+        System.out.println("Bluemix run at localhost");
         response.getWriter().print("Hello World!");
     }
 
